17d2a267e388cddc6c6ecfc28a178efd9eca67fe
marcelosilva351 <83617849+marcelosilva351@users.noreply.github.com>
2021-10-17 15:20:19 -0300

    Update UserController.cs

--- ProjetoApiCompleta/Controllers/UserController.cs
+++ ProjetoApiCompleta/Controllers/UserController.cs
@@ -5,7 +5,37 @@ using System.Threading.Tasks;
 
 namespace ApiPires.Controllers
 {
-    public class UserController
+    [ApiControler]
+    [route("api/users")]
+    public class UserController : ControllerBase
     {
+      private readonly IMapper _mapper;
+      private readonly IUserService _userservice;
+      public UserController(IuserService userservice, IMapper mapper){
+      _userserivce = userserivce;
+      _mapper = mapper;
+      }
+      
+      [HttpPost("RegistrarUsuario')
+      public async Task<ActionResult> Cadastrar([FromBody] CreateUsuarioDTO usuario){
+      var usuarioAdd = _mapper.map<Usuario>(usuario);
+      await _userservice.CadastrarUsuario(usuario);
+      return created("Usuario cadastrado", usuarioAdd);
+      }
+      
+      [HttpPost("Login")]
+      public ActionResult<string> Login([FromBody] UsuarioLoginDTO usuarioLogin)
+      {
+         try{
+          return _userservice.GenerateToken(usuarioLogin);
+         }catch(Exception e)
+         {
+           Return StatusCode(400, e.message);
+         }
+         
+   
+      }
+      
+      }
     }
 }
